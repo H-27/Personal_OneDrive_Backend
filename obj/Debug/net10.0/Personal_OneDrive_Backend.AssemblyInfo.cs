9e01974f99bf7f8b9c729cf3489d5d5f8289f244
H-27 <Jenshuth@t-online.de>
2026-05-28 17:23:47 +0200

    Update dependencies and project files for Personal_OneDrive_Backend
    - Updated Microsoft.Data.Sqlite.Core to version 9.0.2
    - Added NeoSmart.Caching.Sqlite and NeoSmart.Caching.Sqlite.AspNetCore packages, both at version 9.0.1
    - Modified project.assets.json to include new dependencies and their respective paths
    - Updated sourcelink.json to reflect changes in the project structure
    - Updated various cache and metadata files to ensure compatibility with new packages

--- obj/Debug/net10.0/Personal_OneDrive_Backend.AssemblyInfo.cs
+++ obj/Debug/net10.0/Personal_OneDrive_Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Personal_OneDrive_Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+559cb30a6eafafbfaa744f38a6312da132f3d7ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ccd4e5a459497a0d25c924cc50bd5cbdb3a0053")]
 [assembly: System.Reflection.AssemblyProductAttribute("Personal_OneDrive_Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Personal_OneDrive_Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
